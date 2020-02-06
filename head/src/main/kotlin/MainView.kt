fae1e4d0fbd1e0cea6125b135fd0e6c47fdb2bcc
Falko Schumann <falko.schumann@muspellheim.de>
2020-02-06 17:43:15 +0100

    UI-Prototyp angelegt

---
+++ head/src/main/kotlin/MainView.kt
@@ -0,0 +1,16 @@
+import javafx.scene.Parent
+import javafx.scene.control.Label
+import javafx.scene.layout.VBox
+import javafx.scene.text.Font
+
+class MainView() {
+     val root: Parent
+
+    init {
+        root = VBox().apply {
+            children += Label("Hello World").apply {
+                font = Font(font.name, 20.0)
+            }
+        }
+    }
+}
