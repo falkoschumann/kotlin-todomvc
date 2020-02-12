2d582b89a51d9bcb292e0c6e17c9697454a01991
Falko Schumann <falko.schumann@muspellheim.de>
2020-02-12 22:06:22 +0100

    View-Model angelegt

---
+++ head/src/main/kotlin/TodoViewModel.kt
@@ -0,0 +1,43 @@
+/*
+ * TodoMVC
+ * Copyright (c) 2020 Falko Schumann
+ */
+
+package de.muspellheim.todomvc.portal
+
+import javafx.beans.property.BooleanProperty
+import javafx.beans.property.IntegerProperty
+import javafx.beans.property.ObjectProperty
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleObjectProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.StringProperty
+import javafx.collections.FXCollections
+
+class TodoListViewModel {
+    val allCompletedProperty: BooleanProperty = SimpleBooleanProperty()
+    val items = FXCollections.observableArrayList<TodoViewModel>()
+
+    val itemsLeftProperty: IntegerProperty = SimpleIntegerProperty()
+    val filterProperty: ObjectProperty<Filter> = SimpleObjectProperty()
+
+    fun add(todo: String) {
+        TODO()
+    }
+
+    fun clearCompleted() {
+        TODO()
+    }
+
+    enum class Filter { All, Active, Completed }
+}
+
+class TodoViewModel {
+    val completedProperty: BooleanProperty = SimpleBooleanProperty()
+    val textProperty: StringProperty = SimpleStringProperty()
+
+    fun remove() {
+        TODO()
+    }
+}
